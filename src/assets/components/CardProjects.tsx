980daefb2021304dfb6bb6095be7b104e42e4320
FarCodeFR <Timo-44@hotmail.com>
2024-12-31 16:23:33 +0100

    mapper sur les cards (Projects + modification du style

--- src/assets/components/CardProjects.tsx
+++ src/assets/components/CardProjects.tsx
@@ -5,22 +5,28 @@ import "../../models/DataModel";
 function CardProjects() {
 	return (
 		<>
-			{data.map((el) => {
-				return (
-					<section id={el.name} key={el.id} className="container-card-projects">
-						<section>
-							<h2>{el.name}</h2>
+			<section className="container-placement-cards">
+				{data.map((el) => {
+					return (
+						<section
+							id={el.title}
+							key={el.id}
+							className="container-card-projects"
+						>
+							<section>
+								<h2>{el.name}</h2>
+							</section>
+							<section className="container-card-titre">
+								<h3>{el.title}</h3>
+								<h3>info</h3>
+							</section>
+							<picture>
+								<img src={el.image} alt="" />
+							</picture>
 						</section>
-						<section className="container-card-titre">
-							<h3>{el.title}</h3>
-							<h3>info</h3>
-						</section>
-						<picture>
-							<img src={el.image} alt="" />
-						</picture>
-					</section>
-				);
-			})}
+					);
+				})}
+			</section>
 		</>
 	);
 }
