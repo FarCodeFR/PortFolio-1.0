733a3ab44fe4a82323560a135af1ce3eb9ee4773
FarCodeFR <Timo-44@hotmail.com>
2025-04-15 17:48:56 +0200

    Ajout formulaire avec Formspree

--- src/assets/components/Formulaire.tsx
+++ src/assets/components/Formulaire.tsx
@@ -1,36 +1,40 @@
-import { useState } from "react";
 import "../../styles/formulaire.css";
 
 function Formulaire() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [message, setMessage] = useState("");
-
-	const onSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
-		console.log(`Message de ${name} ${email} ${message}`);
-		alert("Message envoyé !");
-	};
-
 	return (
 		<section className="container-formulaire">
 			<div>
 				<h3>Besoin d'informations ?</h3>
 			</div>
-			<form aria-labelledby="form-titre" onSubmit={onSubmit}>
+
+			<form
+				aria-labelledby="form-titre"
+				method="POST"
+				action="https://formspree.io/f/mjkyzjye"
+			>
 				<div className="container-form-div">
 					<label htmlFor="nom">Nom</label>
-					<input placeholder="Dupont" required id="nom" />
+					<input placeholder="Dupont" name="name" required id="nom" />
 				</div>
 				<div className="container-form-div">
 					<label htmlFor="email">Email</label>
-					<input placeholder="Email@gmail.com" required id="email" />
+					<input
+						placeholder="Email@gmail.com"
+						name="email"
+						required
+						id="email"
+					/>
 				</div>
 				<div className="container-form-div">
 					<label htmlFor="message">Message</label>
-					<textarea placeholder="Rédigez votre message" required id="message" />
+					<textarea
+						placeholder="Rédigez votre message"
+						name="message"
+						required
+						id="message"
+					/>
 				</div>
-				<button type="button">Envoyer votre message</button>
+				<button type="submit">Envoyer votre message</button>
 			</form>
 		</section>
 	);
